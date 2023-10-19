da3ae990b7b4903f4f45a89f89abdbf3938c1127
tinkugupta5 <tinku.gupta4@gmail.com>
2023-10-20 00:22:02 +0530

    adding

---
+++ Arrays_Questions/MinMax.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int size;
+    cin >> size;
+    int num[100];
+    for ( int i = 0; i<size; i++)
+    {    
+
+        
+        /* code */
+    }
+    
+
+
+    
+    return 0;
+
+}
