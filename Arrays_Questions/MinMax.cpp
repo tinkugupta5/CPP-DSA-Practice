5a9f8466895641b159c42f6d31a5c45ed46218cc
tinkugupta5 <tinku.gupta4@gmail.com>
2023-10-27 23:42:13 +0530

    adding array scope

--- Arrays_Questions/MinMax.cpp
+++ Arrays_Questions/MinMax.cpp
@@ -15,7 +15,7 @@ int getMax(int num[],int n) {
 }
 
 int getMin (int num[],int n){
-    int min = INT_MIN;
+    int min = INT_MAX;
     for(int i = 0; i<n; i++){
         if(num[i] > min)
         {
---
+++ Oct_2023/arrayScope.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void update(int arr[],int n){
+    cout << "Inside the function" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        /* code */
+        cout << arr[i] << "";
+    } cout << endl;
+    cout << " Going back to main function " << endl;
+}
+
+int main() {
+    int arr[3] = {1,2,3};
+    update(arr,3);
+    //printing the array
+    for(int i=0; i<3; i++) {
+        cout << arr[i] << "";
+    }
+    cout << endl;
+    return 0 ;
+}
