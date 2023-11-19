0d92237c0174d0de689c8836cab6c8ac5b530165
tinkugupta5 <tinku.gupta4@gmail.com>
2023-11-19 23:51:10 +0530

    adding

--- Arrays_Questions/MinMax.cpp
+++ Arrays_Questions/MinMax.cpp
@@ -32,9 +32,8 @@ int main() {
     //taking input in array
     for ( int i = 0; i<size; i++)
     {    
-        cin>>num[i];       
-        
-    }
+        cin>>num[i];               
+    }   
 
     cout << " Maximum value is"<< getMax(num,size)<<endl;
     cout << " Minmum value is"<< getMin(num,size)<<endl;
